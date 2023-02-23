2e673488dd6c16446af39f1b869b1b95e671ccca
knowactor <79815499+knowactor@users.noreply.github.com>
2023-02-23 10:38:51 +0800

    chapter02

---
+++ ch02/02_03.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void butler(void);
+int main(void){
+    printf("I will summon the butler func.\n");
+    butler();
+    printf("Yes Bring me some tea and writeable DVDs.\n");
+    return 0;
+}
+void butler(void){
+    printf("You rang,sir\n");
+}
---
+++ ch02/02_04.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+    int n, n2 ,n3;
+    /*This Program has some errors*/
+    n = 5;
+    n2 = n * n ;
+    n3 = n * n * n;
+    printf("n = %d, n squared = %d , n cubed = %d \n" , n, n2, n3)
+    return 0;
+}
